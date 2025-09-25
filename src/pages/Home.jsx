e014ace9ae04a1a838469c6ce778782233f512f0
Srinithish S M <157304330+Srinithish-dev-lab@users.noreply.github.com>
2025-09-25 20:52:33 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-const apiKey = process.env.REACT_APP_TMDB_KEY;
+const apiKey = import.meta.env.VITE_TMDB_KEY;
 import Moviecard from "../Components/Moviecard"
 
 const Home = () => {
